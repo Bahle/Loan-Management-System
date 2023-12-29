de7af121684a3fddb0bbd4c5bc1eedb3a3107d1e
bahlemakhaya <bahle.makhaya@gmail.com>
2023-12-29 16:49:48 +0200

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 const dbConnection = {
-	user: 'bd033b55c76865',
-	password: 'a3094b27',
-	host: 'eu-cdbr-west-03.cleardb.net',
-	database: 'heroku_521f2ba0fb77bc9',
+	user: '-----------',
+	password: '-----------',
+	host: '-----------',
+	database: '-----------',
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0
